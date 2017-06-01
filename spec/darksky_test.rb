a7655b874b5737bebb9af7d5de52483856e66580
Amy Lai <amyjlai@hotmail.com>
2017-05-31 21:46:55 -0400

    First real commit: adding darksky integration

---
+++ spec/darksky_test.rb
@@ -0,0 +1,33 @@
+require_relative '../lib/darksky-slackbot.rb'
+require 'rspec'
+
+describe "darksky functionality" do
+  it "gets current weather" do
+    result = { 'currently' => { 'summary' => 'Light rain', 'temperature' => 65.8 } }
+    allow(Darksky).to receive(:darksky_info).and_return(result)
+    #better to mock HTTParty, haven't been able to get that to work yet
+    expect(Darksky.weather_now).to eq('Light rain, 66 degrees')
+  end
+
+  it "returns an error message when failing to get current weather" do
+    error_msg = "Sorry, your request could not be completed."
+    allow(Darksky).to receive(:darksky_info).and_return(error_msg)
+    #better to mock HTTParty, haven't been able to get that to work yet
+    expect(Darksky.weather_now).to eq(error_msg)
+  end
+
+  it "gets tomorrow's weather" do
+    tomorrow_summary = 'Light rain starting in the morning, continuing until afternoon.'
+    result = { 'daily' => { 'data' => [{ 'summary' => tomorrow_summary }] } }
+    allow(Darksky).to receive(:darksky_info).and_return(result)
+    #better to mock HTTParty, haven't been able to get that to work yet
+    expect(Darksky.weather_tomorrow).to eq(tomorrow_summary)
+  end
+
+  it "returns an error message when failing to get tomorrow's weather" do
+    error_msg = "Sorry, your request could not be completed."
+    allow(Darksky).to receive(:darksky_info).and_return(error_msg)
+    #better to mock HTTParty, haven't been able to get that to work yet
+    expect(Darksky.weather_tomorrow).to eq(error_msg)
+  end
+end
