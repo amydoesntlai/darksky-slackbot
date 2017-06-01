c78765e61ec704ef4e1f8d86289542f4c19945ba
Amy Lai <amyjlai@hotmail.com>
2017-05-31 20:55:03 -0400

    Work-in-progress commit: organizing darksky functionality

--- lib/darksky-slackbot.rb
+++ lib/darksky-slackbot.rb
@@ -1,14 +1,31 @@
 require 'httparty'
 
-darksky_url = "https://api.darksky.net/forecast/#{ENV['DARKSKY_API_KEY']}/38.9072,-77.0369"
-response = HTTParty.get(darksky_url)
-if response.code == 200
-  current_summary = response['currently']['summary']
-  current_temp = response['currently']['temperature']
-  weather_tomorrow = response['daily']['data'].first['summary']
-  puts current_summary
-  puts current_temp
-  puts weather_tomorrow
-else
-  puts "didn't work"
+class Darksky
+  def self.darksky_info
+    darksky_url = "https://api.darksky.net/forecast/#{ENV['DARKSKY_API_KEY']}/38.9072,-77.0369"
+    response = HTTParty.get(darksky_url)
+    if response.code == 200
+      return response
+    else
+      return "Sorry, your request could not be completed."
+    end
+  end
+
+  def self.weather_now
+    info = darksky_info
+    if info.is_a? String
+      return info
+    end
+    current_summary = info['currently']['summary']
+    current_temp = info['currently']['temperature'].round(0)
+    return "#{current_summary}, #{current_temp} degrees"
+  end
+
+  def self.weather_tomorrow
+    info = darksky_info
+    if info.is_a? String
+      return info
+    end
+    return info['daily']['data'].first['summary']
+  end
 end
