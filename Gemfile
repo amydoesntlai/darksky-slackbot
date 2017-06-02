752541a32b1b66e0abb6efe18e41cd8bce292f23
Amy Lai <amyjlai@hotmail.com>
2017-06-01 22:04:49 -0400

    Work-in-progress commit: preliminary slack call

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source "http://rubygems.org"
 
 gem 'httparty'
 gem 'rspec'
+gem 'slack-ruby-bot'
+gem 'celluloid-io'
--- lib/darksky-slackbot.rb
+++ lib/darksky-slackbot.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'slack-ruby-bot'
 
 class Darksky
   def self.darksky_info
@@ -29,3 +30,14 @@ class Darksky
     return info['daily']['data'].first['summary']
   end
 end
+
+class Slackbot < SlackRubyBot::Bot
+  command 'weather now' do |client, data, match|
+    client.say(text: Darksky.weather_now, channel: data.channel)
+  end
+
+  command 'weather tomorrow' do |client, data, match|
+    client.say(text: Darksky.weather_tomorrow, channel: data.channel)
+  end
+end
+Slackbot.run
